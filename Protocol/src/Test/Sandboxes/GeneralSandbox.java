3df2f8ff0525b024799550d8d34b1631cdb762ef
mvargas33 <mvargas33@protonmail.com>
2021-02-09 14:07:12 -0300

    Only frontend files left

--- Protocol/src/Test/Sandboxes/GeneralSandbox.java
+++
@@ -1,7 +0,0 @@
-package Test.Sandboxes;
-
-public class GeneralSandbox {
-    public static void main(String[] args) {
-
-    }
-}
--- Pruebas/Pruebas_BMS/Arduino/libraries/MPU9250-master/Documents/Readme.md
+++
@@ -1,1 +0,0 @@
-Various documents I have collected over the years regarding operation and use of the MPU9250
