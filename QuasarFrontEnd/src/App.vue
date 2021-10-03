b52cf09142cc6c7d5813bfe6599626951589d246
Jayki-ZX <danteantu5@gmail.com>
2021-10-03 14:11:46 -0300

    restored connection with testclient

--- QuasarFrontEnd/src/App.vue
+++ QuasarFrontEnd/src/App.vue
@@ -12,43 +12,43 @@ export default {
       console.log('broadcast event listened\n')
       // console.log(this.$store)
       console.log(data)
-      this.$store.dispatch('fenix/updateMainData', data.data)
+      this.$store.dispatch('fenix/updateMainData', data)
     },
     kelly_der (data) {
       console.log(data)
-      this.$store.dispatch('fenix/updateKellyDer', data.data)
+      this.$store.dispatch('fenix/updateKellyDer', data)
     },
     kelly_izq (data) {
       console.log(data)
-      this.$store.dispatch('fenix/updateKellyIzq', data.data)
+      this.$store.dispatch('fenix/updateKellyIzq', data)
     },
     bms (data) {
       console.log(data)
-      this.$store.dispatch('fenix/updateBms', data.data)
+      this.$store.dispatch('fenix/updateBms', data)
     },
     bms_temp (data) {
       console.log(data)
-      this.$store.dispatch('fenix/updateBmsTemp', data.data)
+      this.$store.dispatch('fenix/updateBmsTemp', data)
     },
     bms_volt (data) {
       console.log(data)
-      this.$store.dispatch('fenix/updateBmsVolt', data.data)
+      this.$store.dispatch('fenix/updateBmsVolt', data)
     },
     mppt1 (data) {
       console.log(data)
-      this.$store.dispatch('fenix/updateMppt1', data.data)
+      this.$store.dispatch('fenix/updateMppt1', data)
     },
     mppt2 (data) {
       console.log(data)
-      this.$store.dispatch('fenix/updateMppt2', data.data)
+      this.$store.dispatch('fenix/updateMppt2', data)
     },
     mppt3 (data) {
       console.log(data)
-      this.$store.dispatch('fenix/updateMppt3', data.data)
+      this.$store.dispatch('fenix/updateMppt3', data)
     },
     mppt4 (data) {
       console.log(data)
-      this.$store.dispatch('fenix/updateMppt4', data.data)
+      this.$store.dispatch('fenix/updateMppt4', data)
     },
     potencia_mppt (data) {
       this.$store.dispatch('fenix/updatePotenciaMppt', data)
