f1ae4552fb39f3be22948b11ff7ad9604b7263e9
Alonso <alonso.herrera@ug.uchile.cl>
2021-12-28 04:24:03 -0300

    Se agrega vista Alarmas

--- QuasarFrontEnd/src/router/routes.js
+++ QuasarFrontEnd/src/router/routes.js
@@ -10,44 +10,32 @@ const routes = [
   {
     path: '/escolta',
     component: () => import('layouts/AurigaLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/AurigaEscolta.vue') }
-    ]
+    children: [{ path: '', component: () => import('pages/AurigaEscolta.vue') }]
   },
   {
     path: '/fenix',
     component: () => import('layouts/FenixLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Fenix.vue') }
-    ]
+    children: [{ path: '', component: () => import('pages/Fenix.vue') }]
   },
   {
     path: '/fenixbasic',
     component: () => import('layouts/EmptyLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/FenixBasic.vue') }
-    ]
+    children: [{ path: '', component: () => import('pages/FenixBasic.vue') }]
   },
   {
     path: '/aurigabasic',
     component: () => import('layouts/EmptyLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/AurigaBasic.vue') }
-    ]
+    children: [{ path: '', component: () => import('pages/AurigaBasic.vue') }]
   },
   {
     path: '/map',
     component: () => import('layouts/AurigaLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Map.vue') }
-    ]
+    children: [{ path: '', component: () => import('pages/Map.vue') }]
   },
   {
     path: '/graphs',
     component: () => import('layouts/AurigaLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Graphs.vue') }
-    ]
+    children: [{ path: '', component: () => import('pages/Graphs.vue') }]
   },
   {
     path: '/baterias',
@@ -59,9 +47,12 @@ const routes = [
   {
     path: '/pilot',
     component: () => import('layouts/Auriga-PilotLayout.vue'),
-    children: [
-      { path: '/', component: () => import('pages/Pilot.vue') }
-    ]
+    children: [{ path: '/', component: () => import('pages/Pilot.vue') }]
+  },
+  {
+    path: '/alarmas',
+    component: () => import('layouts/AurigaLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Alarmas.vue') }]
   }
 ]
 
@@ -70,9 +61,7 @@ if (process.env.MODE !== 'ssr') {
   routes.push({
     path: '*',
     component: () => import('layouts/AurigaLayout.vue'),
-    children: [
-      { path: '/', component: () => import('pages/Error404.vue') }
-    ]
+    children: [{ path: '/', component: () => import('pages/Error404.vue') }]
   })
 }
 
